cf27c9c6087fd0c3312d4e0952b9d15527448ed8
phu tran <tranvanphu995@outlook.com.vn>
2020-05-06 12:09:03 +0700

    html water-spider

--- ME-SPA/src/app/views/maintenance/water-spider-score-record/water-spider-score-record-list/water-spider-score-record-list.component.ts
+++ ME-SPA/src/app/views/maintenance/water-spider-score-record/water-spider-score-record-list/water-spider-score-record-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pagination } from '../../../../_core/_models/pagination';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { AuditType } from '../../../../_core/_models/audit-type';
 
 @Component({
   selector: 'app-water-spider-score-record-list',
@@ -8,8 +9,13 @@ import { NgxSpinnerService } from 'ngx-spinner';
   styleUrls: ['./water-spider-score-record-list.component.scss']
 })
 export class WaterSpiderScoreRecordListComponent implements OnInit {
-
   pagination: Pagination;
+  timeStart : string;
+  timeEnd : string;
+  pdc : string;line : string;building : string; auditType2 :string;
+  pdcList: string[] = [];
+  lines: string[] = [];
+  buildings: string[] = [];
   constructor( private spinner: NgxSpinnerService
 
               ) { }
@@ -17,6 +23,6 @@ export class WaterSpiderScoreRecordListComponent implements OnInit {
   ngOnInit() {
   }
   pageChanged(){
-    
+
   }
 }
