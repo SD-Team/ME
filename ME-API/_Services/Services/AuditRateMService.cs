392ca532513806ff5d4455d6b1c8901a4324814c
QUANG TRUC ĐINH <quangtruc111@gmail.com>
2020-05-07 08:41:24 +0700

    add controller auditrate

--- ME-API/_Services/Services/AuditRateMService.cs
+++ ME-API/_Services/Services/AuditRateMService.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using ME_API._Repositories.Interface;
@@ -6,27 +7,28 @@ using ME_API._Services.Interface;
 using ME_API.DTO;
 using ME_API.Helpers;
 using ME_API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ME_API._Services.Services
 {
     public class AuditRateMService : IAuditRateMService
     {
-        private readonly IAuditRateMRepository _repo;
+        private readonly IAuditRateMRepository _auditRateMRepository;
         private readonly IMapper _mapper;
         private readonly MapperConfiguration _configMapper;
-        public AuditRateMService(IAuditRateMRepository repo,
+        public AuditRateMService(IAuditRateMRepository auditRateMRepository,
                                  IMapper mapper,
                                  MapperConfiguration configMapper)
         {
-            _repo = repo;
+            _auditRateMRepository = auditRateMRepository;
             _mapper = mapper;
             _configMapper = configMapper;
         }
         public async Task<bool> Add(AuditRateMDto model)
         {
             var auditRateM = _mapper.Map<MES_Audit_Rate_M>(model);
-            _repo.Add(auditRateM);
-            return await _repo.SaveAll();
+            _auditRateMRepository.Add(auditRateM);
+            return await _auditRateMRepository.SaveAll();
         }
 
         public Task<bool> Delete(object id)
@@ -37,6 +39,26 @@ namespace ME_API._Services.Services
         public Task<List<AuditRateMDto>> GetAllAsync()
         {
             throw new System.NotImplementedException();
+        }
+
+        public async Task<List<string>> GetAllAuditType2()
+        {
+            return await _auditRateMRepository.FindAll().GroupBy(x => x.Audit_Type2).Select(x => x.Key).ToListAsync();
+        }
+
+        public async Task<List<string>> GetAllBuilding()
+        {
+            return await _auditRateMRepository.FindAll().GroupBy(x => x.Building).Select(x => x.Key).ToListAsync();
+        }
+
+        public async Task<List<string>> GetAllLine()
+        {
+            return await _auditRateMRepository.FindAll().GroupBy(x => x.Line).Select(x => x.Key).ToListAsync();
+        }
+
+        public async Task<List<string>> GetAllPDC()
+        {
+            return await _auditRateMRepository.FindAll().GroupBy(x => x.PDC).Select(x => x.Key).ToListAsync();
         }
 
         public AuditRateMDto GetById(object id)
