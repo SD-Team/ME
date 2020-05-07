a5307eddd1521052ade89d299c637f16104c3b6c
cuongem huynh <huynhcuongem7597@gmail.com>
2020-05-07 14:21:29 +0700

    #commit

--- ME-API/Controllers/AuditRateController.cs
+++ ME-API/Controllers/AuditRateController.cs
@@ -4,59 +4,49 @@ using ME_API.Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ME_API.Controllers
-{
+namespace ME_API.Controllers {
     // [Authorize]
     [ApiController]
-    [Route("api/[controller]")]
-    public class AuditRateController : ControllerBase
-    {
+    [Route ("api/[controller]")]
+    public class AuditRateController : ControllerBase {
         private readonly IAuditRateMService _auditRateMService;
         private readonly IAuditRateDService _auditRateDService;
         private readonly IAuditRateService _auditRateService;
-        public AuditRateController(IAuditRateDService auditRateDService, IAuditRateMService auditRateMService, IAuditRateService auditRateService)
-        {
+        public AuditRateController (IAuditRateDService auditRateDService, IAuditRateMService auditRateMService, IAuditRateService auditRateService) {
             _auditRateService = auditRateService;
             _auditRateDService = auditRateDService;
             _auditRateMService = auditRateMService;
         }
 
-        [HttpGet("buildings")]
-        public async Task<IActionResult> GetAllBuilding()
-        {
-            var data = await _auditRateMService.GetAllBuilding();
-            return Ok(data);
-        }
-        [HttpGet("lines")]
-        public async Task<IActionResult> GetAllLine()
-        {
-            var data = await _auditRateMService.GetAllLine();
-            return Ok(data);
+        [HttpGet ("buildings")]
+        public async Task<IActionResult> GetAllBuilding () {
+            var data = await _auditRateMService.GetAllBuilding ();
+            return Ok (data);
         }
 
-        [HttpGet("pdcs")]
-        public async Task<IActionResult> GetAllPDC()
-        {
-            var data = await _auditRateMService.GetAllPDC();
-            return Ok(data);
+        [HttpGet ("lines")]
+        public async Task<IActionResult> GetAllLine () {
+            var data = await _auditRateMService.GetAllLine ();
+            return Ok (data);
         }
 
-        [HttpGet("audittype2")]
-        public async Task<IActionResult> GetAllAuditType2()
-        {
-            var data = await _auditRateMService.GetAllAuditType2();
-            return Ok(data);
+        [HttpGet ("pdcs")]
+        public async Task<IActionResult> GetAllPDC () {
+            var data = await _auditRateMService.GetAllPDC ();
+            return Ok (data);
         }
 
-<<<<<<< HEAD
-        
-=======
-        [HttpGet("sixs-list")]
-        public async Task<IActionResult> GetListSixsScoreRecord([FromQuery]PaginationParams paginationParams, SixsScoreRecordParam sixsScoreRecordParam)
-        {
-            var data = await _auditRateService.GetListSixsScoreRecord(paginationParams, sixsScoreRecordParam);
-            return Ok(data);
+        [HttpGet ("audittype2")]
+        public async Task<IActionResult> GetAllAuditType2 () {
+            var data = await _auditRateMService.GetAllAuditType2 ();
+            return Ok (data);
         }
->>>>>>> 822153a961de65d1b11d02131ec3cca7a890cbef
+
+        [HttpGet ("sixs-list")]
+        public async Task<IActionResult> GetListSixsScoreRecord ([FromQuery] PaginationParams paginationParams, SixsScoreRecordParam sixsScoreRecordParam) {
+            var data = await _auditRateService.GetListSixsScoreRecord (paginationParams, sixsScoreRecordParam);
+            return Ok (data);
+        }
+
     }
 }
