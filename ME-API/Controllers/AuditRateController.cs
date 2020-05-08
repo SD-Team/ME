bff97d8bb1af363546fd4bcd8707a06ebe707409
cuongem huynh <huynhcuongem7597@gmail.com>
2020-05-08 08:25:34 +0700

    comit Excel nha truc ml

--- ME-API/Controllers/AuditRateController.cs
+++ ME-API/Controllers/AuditRateController.cs
@@ -1,7 +1,11 @@
+using System;
+using System.IO;
 using System.Threading.Tasks;
+using Aspose.Cells;
 using ME_API._Services.Interface;
 using ME_API.Helpers;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ME_API.Controllers {
@@ -12,10 +16,12 @@ namespace ME_API.Controllers {
         private readonly IAuditRateMService _auditRateMService;
         private readonly IAuditRateDService _auditRateDService;
         private readonly IAuditRateService _auditRateService;
-        public AuditRateController (IAuditRateDService auditRateDService, IAuditRateMService auditRateMService, IAuditRateService auditRateService) {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public AuditRateController (IAuditRateDService auditRateDService, IAuditRateMService auditRateMService, IAuditRateService auditRateService, IWebHostEnvironment webHostEnvironment) {
             _auditRateService = auditRateService;
             _auditRateDService = auditRateDService;
             _auditRateMService = auditRateMService;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         [HttpGet ("buildings")]
@@ -45,15 +51,61 @@ namespace ME_API.Controllers {
         [HttpPost ("sixs-list")]
         public async Task<IActionResult> GetListSixsScoreRecord ([FromQuery] PaginationParams paginationParams, ScoreRecordParam sixsScoreRecordParam) {
             var data = await _auditRateService.GetListSixsScoreRecord (paginationParams, sixsScoreRecordParam);
-            Response.AddPagination(data.CurrentPage, data.PageSize, data.TotalCount, data.TotalPages);
+            Response.AddPagination (data.CurrentPage, data.PageSize, data.TotalCount, data.TotalPages);
             return Ok (data);
         }
 
         [HttpPost ("sme-list")]
         public async Task<IActionResult> GetListSMEScoreRecord ([FromQuery] PaginationParams paginationParams, ScoreRecordParam sixsScoreRecordParam) {
             var data = await _auditRateService.GetLisSMEScoreRecord (paginationParams, sixsScoreRecordParam);
-            Response.AddPagination(data.CurrentPage, data.PageSize, data.TotalCount, data.TotalPages);
+            Response.AddPagination (data.CurrentPage, data.PageSize, data.TotalCount, data.TotalPages);
             return Ok (data);
+        }
+
+        [HttpGet ("ExportExcelSixs")]
+        public async Task<ActionResult> ExportExcelSixsRecord ([FromQuery] PaginationParams paginationParams, ScoreRecordParam sixsScoreRecordParam) {
+            var data = await _auditRateService.GetListSixsScoreRecord (paginationParams, sixsScoreRecordParam, false);
+
+            var path = Path.Combine (_webHostEnvironment.ContentRootPath, "Resources\\Template\\Sixs_Score_Record_Template.xlsx");
+            WorkbookDesigner designer = new WorkbookDesigner ();
+            designer.Workbook = new Workbook (path);
+
+            Worksheet ws = designer.Workbook.Worksheets[0];
+
+            designer.SetDataSource ("result", data);
+            designer.Process ();
+
+            MemoryStream stream = new MemoryStream ();
+            designer.Workbook.Save (stream, SaveFormat.Xlsx);
+
+            // designer.Workbook.Save (path + "Test.xlsx", SaveFormat.Xlsx);
+
+            byte[] result = stream.ToArray ();
+
+            return File (result, "application/xlsx", "Sixs_Score_Record" + DateTime.Now.ToString ("dd_MM_yyyy_HH_mm_ss") + ".xlsx");
+        }
+
+        [HttpGet ("ExportExcelSME")]
+        public async Task<ActionResult> ExportExcelSMERecord ([FromQuery] PaginationParams paginationParams, ScoreRecordParam sixsScoreRecordParam) {
+            var data = await _auditRateService.GetLisSMEScoreRecord (paginationParams, sixsScoreRecordParam, false);
+
+            var path = Path.Combine (_webHostEnvironment.ContentRootPath, "Resources\\Template\\SME_Score_Record_Template.xlsx");
+            WorkbookDesigner designer = new WorkbookDesigner ();
+            designer.Workbook = new Workbook (path);
+
+            Worksheet ws = designer.Workbook.Worksheets[0];
+
+            designer.SetDataSource ("result", data);
+            designer.Process ();
+
+            MemoryStream stream = new MemoryStream ();
+            designer.Workbook.Save (stream, SaveFormat.Xlsx);
+
+            // designer.Workbook.Save (path + "Test.xlsx", SaveFormat.Xlsx);
+
+            byte[] result = stream.ToArray ();
+
+            return File (result, "application/xlsx", "SME_Score_Record" + DateTime.Now.ToString ("dd_MM_yyyy_HH_mm_ss") + ".xlsx");
         }
 
     }
