880dd0949999b1bc6f5e362a50fd905ad036047c
phu tran <tranvanphu995@outlook.com.vn>
2020-05-11 09:47:30 +0700

    Remake

--- ME-SPA/src/app/views/score-record/sixs-score-record-add/sixs-recored-choose-quesion/sixs-recored-choose-quesion.component.ts
+++ ME-SPA/src/app/views/score-record/sixs-score-record-add/sixs-recored-choose-quesion/sixs-recored-choose-quesion.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 })
 export class SixsRecoredChooseQuesionComponent implements OnInit {
   remake: boolean = false;
-  text: any;
+  isLoading = true;
   Test: any[] = [
     {
       id: "1",
@@ -16,6 +16,7 @@ export class SixsRecoredChooseQuesionComponent implements OnInit {
       Rating0: 0,
       Rating1: 0,
       Remake: "",
+
     },
     {
       id: "2",
@@ -23,50 +24,35 @@ export class SixsRecoredChooseQuesionComponent implements OnInit {
       NA: 0,
       Rating0: 0,
       Rating1: 0,
+      Remake: "",
+
     },
   ];
   constructor() {}
   ngOnInit() {}
   cancel() {}
-  chkNa(item) {
+  checkedChang(item,number) {
+    this.isLoading = false;
     this.Test.map((x) => {
       if (x.id == item.id) {
-        x.NA = 1;
-        x.Rating0 = 0;
-        x.Rating1 = 0;
+        if (number==1) {
+          x.NA = 1;
+          x.Rating0 = 0;
+          x.Rating1 = 0;
+          x.Remake = "";
+        } else if (number==2) {
+          x.NA = 0;
+          x.Rating0 = 1;
+          x.Rating1 = 0;
+          x.Remake = item.Remake;
+        } else {
+          x.NA = 0;
+          x.Rating0 = 0;
+          x.Rating1 = 1;
+          x.Remake = "";
+        }
       }
     });
     console.log(1, this.Test);
-  }
-  chkR0(item) {
-    this.Test.map((x) => {
-      if (x.id == item.id) {
-        x.NA = 0;
-        x.Rating0 = 1;
-        x.Rating1 = 0;
-      }
-    });
-    console.log(1, this.Test);
-  }
-  chkR1(item) {
-    this.Test.map((x) => {
-      if (x.id == item.id) {
-        x.NA = 0;
-        x.Rating0 = 0;
-        x.Rating1 = 1;
-      }
-    });
-    console.log(1, this.Test);
-  }
-  ChkRemake(item) {
-    // this.Test.map((x) => {
-    //   if (x.id == item.id) {
-    //     if (x.Rating0 = 1) {
-    //       return true;
-    //     }
-    //     return false;
-    //   }
-    //   return false;
-    // });
   }
 }
