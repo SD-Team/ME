b2f88a0507c41b90d7d60031ddfe1faa62a541c6
phu tran <tranvanphu995@outlook.com.vn>
2020-05-14 15:07:35 +0700

    excel

--- ME-SPA/src/app/views/maintenance/sixs-score-record/sixs-score-record-detail/sixs-score-record-detail.component.ts
+++ ME-SPA/src/app/views/maintenance/sixs-score-record/sixs-score-record-detail/sixs-score-record-detail.component.ts
@@ -1,127 +1,140 @@
-import { Component, OnInit } from '@angular/core';
-import { environment } from '../../../../../environments/environment';
-import { Router } from '@angular/router';
-import { ScoreRecordService } from '../../../../_core/_services/score-record.service';
-import { NgxSpinnerService } from 'ngx-spinner';
-import { AuditRateM } from '../../../../_core/_models/score-record-question';
-import { AuditRateDDetail } from '../../../../_core/_models/score-record-detail';
-import { AlertifyService } from '../../../../_core/_services/alertify.service';
+import { Component, OnInit } from "@angular/core";
+import { environment } from "../../../../../environments/environment";
+import { Router } from "@angular/router";
+import { ScoreRecordService } from "../../../../_core/_services/score-record.service";
+import { NgxSpinnerService } from "ngx-spinner";
+import { AuditRateM } from "../../../../_core/_models/score-record-question";
+import { AuditRateDDetail } from "../../../../_core/_models/score-record-detail";
+import { AlertifyService } from "../../../../_core/_services/alertify.service";
 
 @Component({
-  selector: 'app-sixs-score-record-detail',
-  templateUrl: './sixs-score-record-detail.component.html',
-  styleUrls: ['./sixs-score-record-detail.component.scss']
+  selector: "app-sixs-score-record-detail",
+  templateUrl: "./sixs-score-record-detail.component.html",
+  styleUrls: ["./sixs-score-record-detail.component.scss"],
 })
 export class SixsScoreRecordDetailComponent implements OnInit {
-  urlImage = environment.imageUrl + 'no-image.jpg';
-  url:any = environment.imageUrl;
-  recordId:string ='';
-  auditRateM:AuditRateM={
-    record_ID:'',
-    audit_Type_ID:'',
-    pdc:'',
-    building: '',
-    line: '',
-    audit_Type1: '',
-    audit_Type2: '',
-    me_Pic: '',
-    pd_Resp: '',
-    updated_By: '',
-    updated_Time: '',
+  urlImage = environment.imageUrl + "no-image.jpg";
+  url: any = environment.imageUrl;
+  recordId: string = "";
+  auditRateM: AuditRateM = {
+    record_ID: "",
+    audit_Type_ID: "",
+    pdc: "",
+    building: "",
+    line: "",
+    audit_Type1: "",
+    audit_Type2: "",
+    me_Pic: "",
+    pd_Resp: "",
+    updated_By: "",
+    updated_Time: "",
     record_Date: null,
   };
-  listAuditRateD:AuditRateDDetail[]=[];
-  constructor(private router: Router,
+  listAuditRateD: AuditRateDDetail[] = [];
+  constructor(
+    private router: Router,
     private scoreRecordService: ScoreRecordService,
     private spinner: NgxSpinnerService,
-    private alertify: AlertifyService) { }
+    private alertify: AlertifyService
+  ) {}
 
   ngOnInit() {
-    this.scoreRecordService.currentRecordId.subscribe(recordId=>this.recordId=recordId)
+    this.scoreRecordService.currentRecordId.subscribe(
+      (recordId) => (this.recordId = recordId)
+    );
     this.loadData();
   }
-  loadData()
-  {
+  loadData() {
     this.spinner.show();
-    this.scoreRecordService.getDetailScoreRecord(this.recordId).subscribe(res=>{
-      this.auditRateM =res.auditRateM;
-      this.listAuditRateD=res.listAuditRateD;
-    },(error)=>{
-      this.alertify.error(error);
-    })
+    this.scoreRecordService.getDetailScoreRecord(this.recordId).subscribe(
+      (res) => {
+        this.auditRateM = res.auditRateM;
+        this.listAuditRateD = res.listAuditRateD;
+      },
+      (error) => {
+        this.alertify.error(error);
+      }
+    );
     this.spinner.hide();
   }
-  onSelectFile(event,auditItemId) {
+  onSelectFile(event, auditItemId) {
     if (event.target.files && event.target.files[0]) {
       const reader = new FileReader();
-      var title = event.target.files[0].name.split('.').pop();
+      var title = event.target.files[0].name.split(".").pop();
       var fileZise = event.target.files[0].size;
       var file = event.target.files[0];
-    if(title=='jpg'|| title =='jpeg' || title=='png')
-      {
-      if(fileZise<=2097152){
-        // reader.readAsDataURL(event.target.files[0]); // read file as data url
-        // reader.onload = (event) => { // called once readAsDataURL is completed
-        //   this.url = event.target.result;
-        // };
-        const formData = new FormData();
-        formData.append('file', file);
-        formData.append('recordId', this.recordId);
-        formData.append('auditItemId', auditItemId);
-        this.scoreRecordService.uploadPicture(formData).subscribe(()=>{
-          this.loadData();
-        });
-      }
-      else
-      {
-        this.alertify.error("Images cannot be larger than 2MB");
-      }
-      }
-      else if(title=='mp4')
-      {
-        if(fileZise<=5242880){
+      if (title == "jpg" || title == "jpeg" || title == "png") {
+        if (fileZise <= 2097152) {
+          // reader.readAsDataURL(event.target.files[0]); // read file as data url
+          // reader.onload = (event) => { // called once readAsDataURL is completed
+          //   this.url = event.target.result;
+          // };
+          const formData = new FormData();
+          formData.append("file", file);
+          formData.append("recordId", this.recordId);
+          formData.append("auditItemId", auditItemId);
+          this.scoreRecordService.uploadPicture(formData).subscribe(
+            () => {
+              this.loadData();
+              this.alertify.success(
+                "Upload image of " + auditItemId + " successfully"
+              );
+            },
+            (error) => {
+              this.alertify.success(
+                "Upload image of " + auditItemId + " failed"
+              );
+            }
+          );
+        } else {
+          this.alertify.error("Images cannot be larger than 2MB");
+        }
+      } else if (title == "mp4") {
+        if (fileZise <= 5242880) {
           // reader.readAsDataURL(event.target.files[0]); // read file as data url
           // reader.onload = (event) => { // called once readAsDataURL is completed
           //   this.url = (<FileReader>event.target).result;
-
           //  };
           const formData = new FormData();
-          formData.append('file', file);
-          formData.append('recordId', this.recordId);
-          formData.append('auditItemId', auditItemId);
-          this.scoreRecordService.uploadPicture(formData).subscribe(()=>{
-            this.loadData();
-          } );
-        }
-        else{
+          formData.append("file", file);
+          formData.append("recordId", this.recordId);
+          formData.append("auditItemId", auditItemId);
+          this.scoreRecordService.uploadPicture(formData).subscribe(
+            () => {
+              this.loadData();
+              this.alertify.success(
+                "Upload video of " + auditItemId + " successfully"
+              );
+            },
+            (error) => {
+              this.alertify.error(
+                "Upload video of " + auditItemId + " failed"
+              );
+            }
+          );
+        } else {
           this.alertify.error("Video cannot be larger than 5MB");
         }
-      }
-      else{
+      } else {
         this.alertify.error("Incorrect format");
       }
-
     }
   }
-back()
-{
-  this.router.navigate(['maintenance/6s-score-record'])
-}
-chkImage(uploadPicture)
-{
-  if(uploadPicture!=null)
-  {
-    if(uploadPicture.split('.').pop() =="mp4"){
-      return false;
-    }
-    else
-    {
+  back() {
+    this.router.navigate(["maintenance/6s-score-record"]);
+  }
+  exportExcel() {
+    this.scoreRecordService.exportExcelDetail(this.recordId);
+  }
+  chkImage(uploadPicture) {
+    if (uploadPicture != null) {
+      if (uploadPicture.split(".").pop() == "mp4") {
+        return false;
+      } else {
+        return true;
+      }
+    } else {
       return true;
     }
   }
-  else
-  {
-    return true;
-  }
-}
 }
--- ME-SPA/src/app/views/score-record/sixs-score-record-add/sixs-score-record-add.component.ts
+++ ME-SPA/src/app/views/score-record/sixs-score-record-add/sixs-score-record-add.component.ts
@@ -1,28 +1,37 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { Router } from '@angular/router';
-import { MesOrgService } from '../../../_core/_services/mes-org.service';
-import { ScoreRecordService } from '../../../_core/_services/score-record.service';
-import { ScoreRecordQuestion, AuditRateM, AuditRateModel } from '../../../_core/_models/score-record-question';
-import { AlertifyService } from '../../../_core/_services/alertify.service';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { Router } from "@angular/router";
+import { MesOrgService } from "../../../_core/_services/mes-org.service";
+import { ScoreRecordService } from "../../../_core/_services/score-record.service";
+import {
+  ScoreRecordQuestion,
+  AuditRateM,
+  AuditRateModel,
+} from "../../../_core/_models/score-record-question";
+import { AlertifyService } from "../../../_core/_services/alertify.service";
 @Component({
-  selector: 'app-sixs-score-record-add',
-  templateUrl: './sixs-score-record-add.component.html',
-  styleUrls: ['./sixs-score-record-add.component.scss'],
+  selector: "app-sixs-score-record-add",
+  templateUrl: "./sixs-score-record-add.component.html",
+  styleUrls: ["./sixs-score-record-add.component.scss"],
 })
 export class SixsScoreRecordAddComponent implements OnInit {
   questions: ScoreRecordQuestion[] = [];
-  user: any = JSON.parse(localStorage.getItem('user'));
+  user: any = JSON.parse(localStorage.getItem("user"));
   today: Date = new Date();
   recordDate: Date = new Date();
   pdcs: string[];
-  buildings:  string[];
+  buildings: string[];
   lineIDs: string[];
-  auditType2List: any[]=[];
-  pdc: string; building: string; lineID: string;auditType2:string='';
-  constructor(private router:Router,
-        private mesOrgService: MesOrgService,
-        private scoreService:ScoreRecordService,
-        private alertifyService: AlertifyService) { }
+  auditType2List: any[] = [];
+  pdc: string;
+  building: string;
+  lineID: string;
+  auditType2: string = "";
+  constructor(
+    private router: Router,
+    private mesOrgService: MesOrgService,
+    private scoreService: ScoreRecordService,
+    private alertifyService: AlertifyService
+  ) {}
 
   ngOnInit() {
     this.getAllBuilding();
@@ -31,148 +40,149 @@ export class SixsScoreRecordAddComponent implements OnInit {
     this.getAllType2();
   }
   getAllPdc() {
-    this.mesOrgService.getAllPdc().subscribe(res => {
+    this.mesOrgService.getAllPdc().subscribe((res) => {
       this.pdcs = res;
       this.pdc = this.pdcs[0];
     });
   }
   getAllBuilding() {
-    this.mesOrgService.getAllBuilding().subscribe(res => {
+    this.mesOrgService.getAllBuilding().subscribe((res) => {
       this.buildings = res;
       this.building = this.buildings[0];
     });
   }
   getAllLineId() {
-    this.mesOrgService.getAllLineId().subscribe(res => {
+    this.mesOrgService.getAllLineId().subscribe((res) => {
       this.lineIDs = res;
       this.lineID = this.lineIDs[0];
     });
   }
   getAllType2() {
-    this.scoreService.getAuditType2Score().subscribe(res => {
-      this.auditType2List =  res.map((item) => {
+    this.scoreService.getAuditType2Score().subscribe((res) => {
+      this.auditType2List = res.map((item) => {
         return { id: item, text: item };
       });
       this.auditType2List.unshift({ id: "", text: "Select auditType2" });
     });
-
   }
   back() {
-    this.router.navigate(['maintenance/6s-score-record']);
+    this.router.navigate(["maintenance/6s-score-record"]);
   }
   loadQuestion() {
-    const auditType1 = '6S';
+    const auditType1 = "6S";
     const auditType2 = this.auditType2;
-    this.scoreService.getQuestion(auditType1, auditType2).subscribe(res => {
+    this.scoreService.getQuestion(auditType1, auditType2).subscribe((res) => {
       this.questions = res;
     });
   }
 
   checkChange(item: ScoreRecordQuestion, number) {
     if (number === 0) {
-     item.rating_0 = 1;
-     item.rating_1 = 0;
-     item.rating_2 = 0;
-     item.rate_Na = 0;
-     item.remark = null;
-   }
-   if (number === 1) {
-     item.rating_0 = 0;
-     item.rating_1 = 1;
-     item.rating_2 = 0;
-     item.rate_Na = 0;
-     item.remark = null;
-   }
-   if (number === 2) {
-     item.rating_0 = 0;
-     item.rating_1 = 0;
-     item.rating_2 = 1;
-     item.rate_Na = 0;
-     item.remark = null;
-   }
-   if (number === 3) {
-     item.rating_0 = 0;
-     item.rating_1 = 0;
-     item.rating_2 = 0;
-     item.rate_Na = 1;
-     item.remark = null;
-   }
- }
- save() {
-   if(this.auditType2=="")
-   {
-    this.alertifyService.error("Please option auditType2");
-   }
-   else{
-    let auditRateM = new AuditRateM();
-    auditRateM.pdc = this.pdc;
-    auditRateM.building = this.building;
-    auditRateM.line = this.lineID;
-    auditRateM.audit_Type1 = '6S';
-    auditRateM.audit_Type2 = this.auditType2;
-    auditRateM.audit_Type_ID = this.questions[0].audit_Type_ID;
-    auditRateM.updated_By = this.user.user_Name;
-
-    auditRateM.record_Date = new Date(this.recordDate.toLocaleDateString());
-
-    let param = new AuditRateModel();
-    param.listAuditRateD = this.questions;
-    param.auditRateM = auditRateM;
-
-    // kiểm tra phải trả lời hết các câu hỏi mới được lưu
-    for (let index = 0; index < this.questions.length; index++) {
-      if (this.questions[index].rate_Na === undefined) {
-        this.alertifyService.error('Mời bạn trả lời tất cả các câu hỏi');
-        return;
-      }
+      item.rating_0 = 1;
+      item.rating_1 = 0;
+      item.rating_2 = 0;
+      item.rate_Na = 0;
+      item.remark = null;
     }
-    this.scoreService.saveScoreRecord(param).subscribe(() => {
-        this.alertifyService.success('success');
-        this.router.navigate(['maintenance/6s-score-record']);
-    }, (error) => {
-      this.alertifyService.error(error);
-    });
-   }
-}
-saveAndNew(){
-  if(this.auditType2=="")
-  {
-   this.alertifyService.error("Please option auditType2");
+    if (number === 1) {
+      item.rating_0 = 0;
+      item.rating_1 = 1;
+      item.rating_2 = 0;
+      item.rate_Na = 0;
+      item.remark = null;
+    }
+    if (number === 2) {
+      item.rating_0 = 0;
+      item.rating_1 = 0;
+      item.rating_2 = 1;
+      item.rate_Na = 0;
+      item.remark = null;
+    }
+    if (number === 3) {
+      item.rating_0 = 0;
+      item.rating_1 = 0;
+      item.rating_2 = 0;
+      item.rate_Na = 1;
+      item.remark = null;
+    }
   }
-  else{
-   let auditRateM = new AuditRateM();
-   auditRateM.pdc = this.pdc;
-   auditRateM.building = this.building;
-   auditRateM.line = this.lineID;
-   auditRateM.audit_Type1 = '6S';
-   auditRateM.audit_Type2 = this.auditType2;
-   auditRateM.audit_Type_ID = this.questions[0].audit_Type_ID;
-   auditRateM.updated_By = this.user.user_Name;
+  save() {
+    if (this.auditType2 == "") {
+      this.alertifyService.error("Please option auditType2");
+    } else {
+      let auditRateM = new AuditRateM();
+      auditRateM.pdc = this.pdc;
+      auditRateM.building = this.building;
+      auditRateM.line = this.lineID;
+      auditRateM.audit_Type1 = "6S";
+      auditRateM.audit_Type2 = this.auditType2;
+      auditRateM.audit_Type_ID = this.questions[0].audit_Type_ID;
+      auditRateM.updated_By = this.user.user_Name;
 
-   auditRateM.record_Date = new Date(this.recordDate.toLocaleDateString());
+      auditRateM.record_Date = new Date(this.recordDate.toLocaleDateString());
 
-   let param = new AuditRateModel();
-   param.listAuditRateD = this.questions;
-   param.auditRateM = auditRateM;
+      let param = new AuditRateModel();
+      param.listAuditRateD = this.questions;
+      param.auditRateM = auditRateM;
 
-   // kiểm tra phải trả lời hết các câu hỏi mới được lưu
-   for (let index = 0; index < this.questions.length; index++) {
-     if (this.questions[index].rate_Na === undefined) {
-       this.alertifyService.error('Mời bạn trả lời tất cả các câu hỏi');
-       return;
-     }
-   }
-   this.scoreService.saveScoreRecord(param).subscribe(() => {
-    this.alertifyService.success('success');
-      this.questions =[];
-      this.auditType2="";
-   }, (error) => {
-     this.alertifyService.error(error);
-   });
+      // kiểm tra phải trả lời hết các câu hỏi mới được lưu
+      for (let index = 0; index < this.questions.length; index++) {
+        if (this.questions[index].rate_Na === undefined) {
+          this.alertifyService.error("Please answer all the questions");
+          return;
+        }
+      }
+      this.scoreService.saveScoreRecord(param).subscribe(
+        () => {
+          this.alertifyService.success("success");
+          this.router.navigate(["maintenance/6s-score-record"]);
+        },
+        (error) => {
+          this.alertifyService.error(error);
+        }
+      );
+    }
   }
-}
-  auditType2Change(){
-  this.loadQuestion();
+  saveAndNew() {
+    if (this.auditType2 == "") {
+      this.alertifyService.error("Please option auditType2");
+    } else {
+      let auditRateM = new AuditRateM();
+      auditRateM.pdc = this.pdc;
+      auditRateM.building = this.building;
+      auditRateM.line = this.lineID;
+      auditRateM.audit_Type1 = "6S";
+      auditRateM.audit_Type2 = this.auditType2;
+      auditRateM.audit_Type_ID = this.questions[0].audit_Type_ID;
+      auditRateM.updated_By = this.user.user_Name;
+
+      auditRateM.record_Date = new Date(this.recordDate.toLocaleDateString());
+
+      let param = new AuditRateModel();
+      param.listAuditRateD = this.questions;
+      param.auditRateM = auditRateM;
+
+      // kiểm tra phải trả lời hết các câu hỏi mới được lưu
+      for (let index = 0; index < this.questions.length; index++) {
+        if (this.questions[index].rate_Na === undefined) {
+          this.alertifyService.error("Please answer all the questions");
+          return;
+        }
+      }
+      this.scoreService.saveScoreRecord(param).subscribe(
+        () => {
+          this.alertifyService.success("success");
+          this.questions = [];
+          this.auditType2 = "";
+        },
+        (error) => {
+          this.alertifyService.error(error);
+        }
+      );
+    }
+  }
+  auditType2Change() {
+    this.loadQuestion();
   }
   cancel() {
     this.loadQuestion();
